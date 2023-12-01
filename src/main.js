e7c1e5324f57893c656c2235a1dd633937a58f02
allmightdude <sabzi_f@yahoo.com>
2023-12-02 00:04:43 +0330

    chore: base styles

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
 import "@style/main.scss";
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  spaceBetween: 10,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
